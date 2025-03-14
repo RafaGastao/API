df375989333610fa8386807b1bff0283af552532
RafaGastao <dev.rafaelgastao@gmail.com>
2025-03-14 19:44:35 -0300

    conectou dados da api com banco de dados

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-from flask import Flask,request
+import sqlite3
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-import sqlite3
 
-# @app.route("/pagar")
-# def exibir_mensagem():
-#     return "<h1>Pagar as pessoas, faz bem as pessoas!!!</h1>"
+@app.route("/pagar")
+def exibir_mensagem():
+    return "<h1>Pagar as pessoas, faz bem as pessoas!!!</h1>"
 
-# @app.route("/femandaopix")
-# def manda_pix():
-#     return "<h2>Se a tela apagou, tá devendo!!!<h2>"
 
-# @app.route("/comidas")
-# def comida():
-#     return "<h2>Tomato á milanesa"
+# @app.route("/calote")
+# def manda_o_pix():
+#     return "<h2> se a ela apagou, tá devendo!!!</h2>"
+
 
 def init_db():
     # sqlite3 crie o arquivo database.db e se conecte com a variável conn (connection)
@@ -25,24 +23,35 @@ def init_db():
                      titulo TEXT NOT NULL,
                      categoria TEXT NOT NULL,
                      autor TEXT NOT NULL,
-                     imagem_url TEXT NOT NULL
+                     image_url TEXT NOT NULL
                      )
         """)
 
 
 init_db()
 
-@app.route("/doar", methods = ["POST"])
-def doar(): 
 
+@app.route("/doar", methods=["POST"])
+def doar():
     dados = request.get_json()
 
     titulo = dados.get("titulo")
     categoria = dados.get("categoria")
     autor = dados.get("autor")
-    imagem_url = dados.get("imagem_url")
+    image_url = dados.get("image_url")
 
-# Se o arquivo app.py for o arquivo principal da nossa aplicação,
-# rode a api no modo de depuração
+    if not titulo or not categoria or not autor or not image_url:
+        return jsonify({"erro": "Todos os campos são obrigatórios"}), 400
+
+    with sqlite3.connect("database.db") as conn:
+        conn.execute(f"""
+        INSERT INTO LIVROS(titulo,categoria,autor,image_url)
+        VALUES("{titulo}","{categoria}","{autor}","{image_url}")
+""")
+
+    conn.commit()
+    return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
+
+
 if __name__ == "__main__":
     app.run(debug=True)
